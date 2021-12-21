9c9625b60e197d0ca776b6ea29e5d063c3f7417f
AbhishekDeb70 <abhistf@gmail.com>
2021-12-21 23:54:08 +0530

    new

--- app.js
+++ app.js
@@ -59,34 +59,27 @@ app.get("/:id", (req, res) => {
     const page = parseInt (req.params.id);
     //let lp = 0;
     let prevpage=page-1;
+    let prevPage=true;
+    let nextpage=true;
     //let nextpage=page;
     //var per_page = parseInt (req.params.id);
         knex("log").paginate({
          perPage: 5,
-         currentPage: page,
+         currentPage: page
          }).then(rows => {
          console.log("here")
          console.log(rows.pagination)
-         //console.log(per_page)
-         let renderData = {}
-         if(rows.pagination.currentPage>=1 && rows.pagination.currentPage<=rows.pagination.lastPage)
-            {
-                if(rows.pagination.currentPage == 1){
-                   const xp=true;
-                    console.log("@@@@@@@@", rows);
-                   renderData = {users:rows.data,currentPage:page,xp}
-               }
-              else if(rows.pagination.currentPage == +rows.pagination.lastPage)
-               {
-                   const np=true;
-                   renderData={users:rows.data,prevpage,np};
-               }
-               console.log("*******", rows);
-            renderData = {users:rows.data,currentPage:page+1,prevpage};
-            }
-            else {}
-            console.log("#########", renderData)
-            res.render("home",{users:rows.data,currentPage:page+1,prevpage});
+            
+            if(rows.pagination.currentPage == 1){
+                console.log("inn");
+                res.render("home",{users:rows.data,nextpage:true,prevPage:false,currentPage:page+1});
+            } else if(rows.pagination.lastPage == rows.pagination.currentPage){
+                //console.log('joooo')
+                res.render("home",{users:rows.data,nextpage:false,prevPage:true,prevpage});
+            }else
+                res.render("home",{users:rows.data,nextpage:true,prevPage:true,currentPage:page+1,prevpage});
+            
+            //res.render("home",{users:rows.data,currentPage:page+1,prevpage});
             
         })
     //} 
